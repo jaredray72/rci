30a8c571226b76ad3dd1d3a0615a56b88e7e37aa
jaredray72 <f100_55@yahoo.com>
2016-09-20 16:10:29 -0400

    yes

---
+++ rci.R
@@ -0,0 +1,24 @@
+2<3 & 2>3
+1:5
+5:20
+q=c(1,3,2,1,3,5)
+q*.1
+Q=rep(12,3)
+Q
+h=seq(1,100,by=5);h
+H=seq(1,10,length(20))
+mat2=matrix(1)
+edit(mat2)
+re=.Last.value
+82*Jared
+erf=.Last.value
+te=matrix( sample(1:50,size = 25),5,5)
+foo =function(x){
+3*x
+}
+
+
+foo(3)
+
+shit!~\
+asdf
